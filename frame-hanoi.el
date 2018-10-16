96ae2fee848505a6cbc045fa6e4b0fafc286a124
Mitchell Perilstein <mitchell.perilstein@thingworx.com>
2018-10-16 09:14:30 -0400

    make frames

--- frame-hanoi.el
+++ frame-hanoi.el
@@ -2,22 +2,46 @@
 
 ; (frame-geometry)
 
+; pixels
+;(display-pixel-height)
+;900
+;(display-pixel-width)
+;1440
 
-(display-pixel-height)
-
-(defvar fh--frame-sizes '((20 20) (30 30) (40 40))
-  "(WIDTH HEIGHT) of each frame (disk)")
-
-(defvar fh--pole-locations (let ((w (display-pixel-width))
-				 (h (display-pixel-height)))
-		    (list (cons
-
-(frame-width)
+; chars
+; (frame-height)
+; (frame-width)
 
 
-(defconst
+(defvar fh--frame-sizes '((50 20) (30 10) (60 30))
+  "(WIDTH HEIGHT) character dimensions of each frame (disk)")
 
-(make-frame '((name . "one") (width . 50)  (height . 20) (minibuffer . nil)))
+(defvar fh--pole-locations (let ((x (/ (display-pixel-width) 4))
+				 (y (/ (display-pixel-height) 3)))
+		    (list
+		     (cons x y)
+		     (cons (* 2 x) y)
+		     (cons (* 3 x) y)))
+  "List of (X Y) pixel locations of each pole.")
+
+(defun fh--make-frame (name x y h w)
+  (let ((newframe (make-frame (list (cons 'name name)
+				    (cons 'width w)
+				    (cons 'height h)
+				    (cons 'minibuffer nil))))
+	(newbuf (get-buffer-create (format "*frame-hanoi-%s*" name))))
+    (set-frame-position (window-frame) x y)))
+
+
+
+(fh--make-frame "foo" 10 20 30 40)
+
+
+
+(defun fh--make-frames ()
+  (mapcar #'fh--make-frame
+
+
 
 (loop for x from 1 to 3700 by 100
       do (progn
